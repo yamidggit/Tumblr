e17354cf76730c0ef416ba7c5d0761dba551cd1b
Yamilet <yamidg@yahoo.com>
2016-07-24 08:37:22 +0000

    add devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.default_url_options = { host: 'https://ide.c9.io/yamidgc9/ruby-on-rails', port: 8080 }
 end
